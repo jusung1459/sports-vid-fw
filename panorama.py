f06c6230e4e860342216bc93a1efea7eb3624e52
Beagle User <debian@BeagleBone.localdomain>
2025-06-03 05:06:12 +0000

    setup

---
+++ panorama.py
@@ -0,0 +1,8 @@
+import cv2
+cap = cv2.VideoCapture(1)
+while True:
+   status, photo = cap.read()
+   cv2.imshow("Streaming Video", photo)
+   if cv2.waitKey(10) == 13:
+      break
+cv2.destroyAllWindows()
